5be5d0284db3ac9d3a0702b2b7f709ac2ef5af7e
Yalin Li <zoe.yalin.li@gmail.com>
2021-05-09 16:14:15 -0500

    remove qsdsan version in requirements

--- setup.py
+++ setup.py
@@ -17,14 +17,14 @@ from setuptools import setup
 setup(
     name='exposan',
     packages=['exposan'],
-    version='0.0.13',
+    version='0.0.15',
     license='UIUC',
     author='Quantitative Sustainable Design Group',
     author_email='quantitative.sustainable.design@gmail.com',
     description='Exposition of sanitation and resource recovery systems',
     long_description=open('README.rst').read(),
     url="https://github.com/QSD-Group/EXPOsan",
-    install_requires=['qsdsan==0.0.13',],
+    install_requires=['qsdsan',],
     package_data=
         {'exposan': [
                     'bwaise/*',
