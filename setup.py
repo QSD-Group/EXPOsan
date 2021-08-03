4e9086467c08c4dbba7d80ab804b36edd19b00af
Yalin Li <zoe.yalin.li@gmail.com>
2021-08-03 09:50:50 -0500

    update version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 setup(
     name='exposan',
     packages=['exposan'],
-    version='0.0.18',
+    version='0.0.19',
     license='UIUC',
     author='Quantitative Sustainable Design Group',
     author_email='quantitative.sustainable.design@gmail.com',
