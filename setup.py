0dc19f16592fabc0110325ff6573568e160ac452
Yalin Li <zoe.yalin.li@gmail.com>
2021-08-03 10:33:42 -0500

    add tests

--- setup.py
+++ setup.py
@@ -17,14 +17,14 @@ from setuptools import setup
 setup(
     name='exposan',
     packages=['exposan'],
-    version='0.0.19',
+    version='0.0.20',
     license='UIUC',
     author='Quantitative Sustainable Design Group',
     author_email='quantitative.sustainable.design@gmail.com',
     description='Exposition of sanitation and resource recovery systems',
     long_description=open('README.rst').read(),
     url="https://github.com/QSD-Group/EXPOsan",
-    install_requires=['qsdsan',],
+    install_requires=['qsdsan>=0.2.15',],
     package_data=
         {'exposan': [
                     'bwaise/*',
---
+++ tests/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+EXPOsan: Exposition of sanitation and resource recovery systems
+
+This module is developed by:
+    Yalin Li <zoe.yalin.li@gmail.com>
+
+This module is under the University of Illinois/NCSA Open Source License.
+Please refer to https://github.com/QSD-Group/EXPOsan/blob/main/LICENSE.txt
+for license details.
+'''
+
+from . import (
+    test_bwaise,
+    )
+
+
+__all__ = (
+    *test_bwaise.__all__,
+    )
+
