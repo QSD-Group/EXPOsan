0865b327dba5ba72f2eaef12ed9a04ca31a9b23a
Yalin <zoe.yalin.li@gmail.com>
2022-03-03 21:40:16 -0600

    bump up version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 setup(
     name='exposan',
     packages=['exposan'],
-    version='1.1.0',
+    version='1.1.1',
     license='UIUC',
     author='Quantitative Sustainable Design Group',
     author_email='quantitative.sustainable.design@gmail.com',
@@ -28,7 +28,7 @@ setup(
         'QSDsan': 'https://github.com/QSD-Group/QSDsan',
         'QSDsan documentation': 'https://qsdsan.readthedocs.io/',
     },
-    install_requires=['qsdsan>=1.1.2',],
+    install_requires=['qsdsan>=1.1.3',],
     package_data=
         {'exposan': [
                     'bsm1/*',
