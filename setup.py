9235ba1c414766e07dafa39e972ae478f80d1d0a
Yalin Li <mailto.yalin.li@gmail.com>
2022-08-14 08:59:32 -0500

    release v1.2.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 setup(
     name='exposan',
     packages=['exposan'],
-    version='1.1.5',
+    version='1.2.0',
     license='UIUC',
     author='Quantitative Sustainable Design Group',
     author_email='quantitative.sustainable.design@gmail.com',
@@ -28,7 +28,7 @@ setup(
         'QSDsan': 'https://github.com/QSD-Group/QSDsan',
         'QSDsan documentation': 'https://qsdsan.readthedocs.io/',
     },
-    install_requires=['qsdsan>=1.1.4',],
+    install_requires=['qsdsan>=1.2.0',],
     package_data=
         {'exposan': [
                     'adm/*',
