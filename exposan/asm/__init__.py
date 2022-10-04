8b285be22971832e616aa9620f1aa23000f219e6
Yalin Li <mailto.yalin.li@gmail.com>
2022-10-04 09:39:17 -0500

    update the asm module and its test to be using the new `load` feature routine

--- exposan/asm/__init__.py
+++ exposan/asm/__init__.py
@@ -5,6 +5,7 @@
 EXPOsan: Exposition of sanitation and resource recovery systems
 
 This module is developed by:
+    
     Yalin Li <mailto.yalin.li@gmail.com>
 
 This module is under the University of Illinois/NCSA Open Source License.
@@ -18,7 +19,68 @@ data_path = os.path.join(asm_path, 'data')
 results_path = os.path.join(asm_path, 'results')
 # To save simulation data
 if not os.path.isdir(results_path): os.mkdir(results_path)
-del os
+
+
+# %%
+
+# =============================================================================
+# Universal parameters
+# =============================================================================
+
+# Streams
+Temp = 273.15+20    # temperature [K]
+Q = 18446           # influent flowrate [m3/d]
+
+# Tanks
+V_an = 1000    # anoxic zone tank volume [m3]
+V_aer = 1333    # aerated zone tank volume [m3]
+
+
+# %%
+
+# =============================================================================
+# Load the components, process model, and system
+# =============================================================================
 
 from . import systems
-from .systems import *
+from .systems import create_system
+
+_system_loaded = False
+_loaded_pc_model = None
+_loaded_aerated = None
+def load(process_model='ASM1', aerated=False, reload=False,
+         asm_kwargs=None, inf_kwargs=None, init_conds=None, flowsheet=None):
+    global _system_loaded, _loaded_pc_model, _loaded_aerated
+    if (process_model!=_loaded_pc_model) or (_loaded_aerated!=aerated): reload = True
+    if reload:
+        global cmps, components, asm, sys
+        sys = create_system(
+            process_model=process_model,
+            aerated=aerated,
+            asm_kwargs=asm_kwargs,
+            inf_kwargs=inf_kwargs,
+            init_conds=init_conds,
+            flowsheet=flowsheet
+            )
+        CSTR = sys.flowsheet.unit.CSTR
+        cmps = components = CSTR.components
+        asm = CSTR.suspended_growth_model
+        _loaded_pc_model = process_model
+        _loaded_aerated = aerated
+    dct = globals()
+    dct.update(sys.flowsheet.to_dict())
+    _system_loaded = True
+
+
+def __getattr__(name):
+    if not _system_loaded:
+        raise AttributeError(f'module "{__name__}" not yet loaded, '
+                             f'load module with `{__name__}.load()`.')
+
+
+__all__ = (
+    'asm_path',
+    'data_path',
+    'results_path',
+    *systems.__all__,
+)
--- tests/test_asm.py
+++ tests/test_asm.py
@@ -17,28 +17,27 @@ __all__ = ('test_asm',)
 def test_asm():
     import numpy as np
     from exposan import asm
-    asm1_an = asm.create_asm_validation_system(process_model='ASM1', aerated=False)
-    asm1_aer = asm.create_asm_validation_system(process_model='ASM1', aerated=True)
-    asm2d_an = asm.create_asm_validation_system(process_model='ASM2d', aerated=False)
-    asm2d_aer = asm.create_asm_validation_system(process_model='ASM2d', aerated=True)
-
     t = 10
     t_step = 0.5
-    syses = (asm1_an, asm1_aer, asm2d_an, asm2d_aer)
-    states = (
-        dict(S_S=69.50000000001846, X_S=202.7347508802057, X_BH=27.719183825843597, S_NH=32.04570170429094),
-        dict(S_S=64.32684709384519, X_S=196.16142052970068, X_BH=35.46730553472383, S_NH=31.745090457273598),
-        dict(S_A=4.114072165874767, X_S=123.55649395125677, X_H=29.519897578137343, S_NH4=16.157541424757596),
-        dict(S_A=0.578711484217068, X_S=118.32521252898434, X_H=37.24707568599702, S_NH4=15.914049030156331),
-        )
-    for sys, state in zip(syses, states):
-        sys.simulate(state_reset_hook='reset_cache',
-                     t_span=(0, t),
-                     t_eval=np.arange(0, t+t_step, t_step),
-                     method='BDF')
-        CSTR = sys.flowsheet.unit.CSTR
-        for k, v in state.items():
-            assert np.isclose(v, CSTR.state[k], rtol=0.01)
+    states_dct = {
+        ('ASM1', False): dict(S_S=69.50000000001846, X_S=202.7347508802057, X_BH=27.719183825843597, S_NH=32.04570170429094),
+        ('ASM1', True): dict(S_S=64.32684709384519, X_S=196.16142052970068, X_BH=35.46730553472383, S_NH=31.745090457273598),
+        ('ASM2d', False): dict(S_A=4.114072165874767, X_S=123.55649395125677, X_H=29.519897578137343, S_NH4=16.157541424757596),
+        ('ASM2d', True): dict(S_A=0.578711484217068, X_S=118.32521252898434, X_H=37.24707568599702, S_NH4=15.914049030156331),
+        }
+    
+    for pc_model in ('ASM1', 'ASM2d'):
+        for aerated in (False, True):
+            asm.load(process_model=pc_model, aerated=aerated)
+            sys = asm.sys
+            states = states_dct[(pc_model, aerated)]
+            sys.simulate(state_reset_hook='reset_cache',
+                         t_span=(0, t),
+                         t_eval=np.arange(0, t+t_step, t_step),
+                         method='BDF')
+            CSTR = sys.flowsheet.unit.CSTR
+            for k, v in states.items():
+                assert np.isclose(v, CSTR.state[k], rtol=0.01)
 
 if __name__ == '__main__':
     test_asm()
