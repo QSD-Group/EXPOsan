289022bf9a3fd6b52d47d6331a5f90a92d67004b
Yalin Li <mailto.yalin.li@gmail.com>
2022-10-04 12:16:29 -0500

    update the adm module and its test to use the new `load` routine

--- exposan/adm/__init__.py
+++ exposan/adm/__init__.py
@@ -23,6 +23,32 @@ del os
 from . import system
 from .system import *
 
+_system_loaded = False
+def load(reload=False, adm_kwargs={}, inf_kwargs={}, init_conds={}, flowsheet=None):
+    global _system_loaded
+    if not _system_loaded: reload = True
+    if reload:
+        global cmps, components, adm, sys
+        sys = create_system(
+            inf_kwargs=inf_kwargs,
+            adm_kwargs=adm_kwargs,
+            init_conds=init_conds,
+            flowsheet=flowsheet,
+            )
+        AD = sys.flowsheet.unit.AD
+        cmps = components = AD.components
+        adm = AD.model
+    dct = globals()
+    dct.update(sys.flowsheet.to_dict())
+    _system_loaded = True
+
+
+def __getattr__(name):
+    if not _system_loaded:
+        raise AttributeError(f'module "{__name__}" not yet loaded, '
+                             f'load module with `{__name__}.load()`.')
+
+
 from . import model
 from .model import *
 
