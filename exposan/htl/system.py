44bd5563f5e01546ed9c0172fe468971bad33231
Jianan Feng <jiananf2@illinois.edu>
2022-10-05 19:04:35 -0500

    Update system.py

--- exposan/htl/system.py
+++ exposan/htl/system.py
@@ -15,25 +15,29 @@ for license details.
 
 import qsdsan as qs
 import exposan.htl._sanunits as su
+from qsdsan import sanunits as suu
 from exposan.htl._components import create_components
 
 
 cmps = create_components()
 
-sludge = qs.WasteStream('sludge',Sludge_lipid=308,Sludge_protein=464,Sludge_carbo=228,units='kg/hr')
+sludge = qs.WasteStream('sludge',Sludge_lipid=308,Sludge_protein=464,Sludge_carbo=228,H2O=4000, units='kg/hr',T=25+273.15)
 acidforP = qs.WasteStream('acidforP',H2SO4=33,units='kg/hr')
 supply_mgcl2 = qs.WasteStream('MgCl2',MgCl2=66,units='kg/hr')
 acidforN = qs.WasteStream('acidforN', H2SO4=114,units='kg/hr')
 
-htl = su.HTL('htl',ins=sludge,outs=('biocrude','aqueous','offgas','biochar'))
-ht = su.HT('ht',ins=htl-0,outs=('biooil', 'fuelgas_HT', 'char'))
-acidex = su.AcidExtraction('acidex',ins=(htl-3,acidforP),outs=('residual','H3PO4'))
-M1 = su.HTLmixer('M1',ins=(htl-1,acidex-1),outs=('mixture',))
-strupre = su.StruvitePrecipitation('strupre',ins=(M1-0,supply_mgcl2),outs=('struvite','chgfeed'))
-chg = su.CHG('chg',ins=strupre-1,outs=('fuelgas_CHG','chgeffluent'))
-memdis = su.MembraneDistillation('memdis',ins=(chg-1,acidforN),outs=('AmmoniaSulfate','ww'))
+HX1 = suu.HXutility('HX1',ins=sludge,outs=('HX1out',),T=350+273.15)
+HX2 = suu.HXprocess
 
-sys = qs.System('sys',path=(htl,ht,acidex,M1,strupre,chg,memdis))
+HTL = su.HTL('HTL',ins=HX1-0,outs=('biocrude','aqueous','offgas','biochar'))
+HT = su.HT('HT',ins=HTL-0,outs=('biooil', 'fuelgas_HT', 'char'))
+Acidex = su.AcidExtraction('Acidex',ins=(HTL-3,acidforP),outs=('residual','H3PO4'))
+M1 = su.HTLmixer('M1',ins=(HTL-1,Acidex-1),outs=('mixture',))
+Strupre = su.StruvitePrecipitation('Strupre',ins=(M1-0,supply_mgcl2),outs=('struvite','chgfeed'))
+CHG = su.CHG('CHG',ins=Strupre-1,outs=('fuelgas_CHG','chgeffluent'))
+MemDis = su.MembraneDistillation('MemDis',ins=(CHG-1,acidforN),outs=('AmmoniaSulfate','ww'))
+
+sys = qs.System('sys',path=(HX1,HTL,HT,Acidex,M1,Strupre,CHG,MemDis))
     
 sys.simulate()
 sys.diagram()
