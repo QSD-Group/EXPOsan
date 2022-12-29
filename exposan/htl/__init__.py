b29f65d64df2f889ab4f2b674c79252cb863e22c
Yalin Li <zoe.yalin.li@gmail.com>
2022-12-29 11:52:37 -0600

    move uncertainty/sensitivity analyses codes to __init__; minor cleanup

--- exposan/htl/__init__.py
+++ exposan/htl/__init__.py
@@ -14,7 +14,7 @@ Please refer to https://github.com/QSD-Group/EXPOsan/blob/main/LICENSE.txt
 for license details.
 '''
 
-import os, qsdsan as qs
+import os, pandas as pd, qsdsan as qs
 from qsdsan.utils import auom
 from exposan.utils import _init_modules
 
@@ -44,23 +44,6 @@ def _load_components(reload=False):
         qs.set_thermo(components)
         _components_loaded = True
 
-# from . import _process_settings
-# from ._process_settings import *
-
-# from . import _tea
-# from ._tea import *
-
-# from . import systems
-# from .systems import *
-# _system_loaded = False
-# def _load_system(configuration='baseline'):
-#     global sys, tea, lca, _system_loaded
-#     sys = create_system(configuration)
-#     tea = sys.TEA
-#     lca = sys.LCA
-#     _system_loaded = True
-
-
 from . import _process_settings
 from ._process_settings import *
 
@@ -69,13 +52,6 @@ from ._tea import *
 
 from . import systems
 from .systems import *
-
-# def _load_system(configuration='baseline'):
-#     global sys, tea, lca, _system_loaded
-#     sys = create_system(configuration)
-#     tea = sys.TEA
-#     lca = sys.LCA
-#     _system_loaded = True
 
 _system_loaded = False
 def load(configuration='baseline'):
@@ -99,13 +75,52 @@ def __getattr__(name):
 from . import models
 from .models import *
 
+def simulate_and_save(model, N, rule='L', seed=None, path='',
+                      include_spearman=True, **spearman_kwargs):
+    samples = model.sample(N=N, rule=rule, seed=seed)
+    model.load_samples(samples)
+    model.evaluate()
+    idx = len(model.parameters)
+    parameters = model.table.iloc[:, :idx]
+    results = model.table.iloc[:, idx:]
+    percentiles = results.quantile([0, 0.05, 0.25, 0.5, 0.75, 0.95, 1])
+    if include_spearman:
+        kwargs = {'nan_policy': 'omit'}
+        kwargs.update(spearman_kwargs)
+        r_df, p_df = qs.stats.get_correlations(model, kind='Spearman', **kwargs)
+    
+    ID = model.system.flowsheet.ID
+    N = model.table.shape[0]
+    path = path or os.path.join(results_path, f'_{ID}_{N}.xlsx')
+    with pd.ExcelWriter(path) as writer:
+        parameters.to_excel(writer, sheet_name='Parameters')
+        results.to_excel(writer, sheet_name='Results')
+        percentiles.to_excel(writer, sheet_name='Percentiles')
+        if include_spearman:
+            r_df.to_excel(writer, sheet_name='Spearman_r')
+            p_df.to_excel(writer, sheet_name='Spearman_p')
+    writer.save()
+
+
 __all__ = (
     'htl_path',
     'data_path',
     'results_path',
+    'simulate_and_save',
     *_components.__all__,
     *_process_settings.__all__,
     *_tea.__all__,
     *systems.__all__,
     *models.__all__,
 )
+
+
+
+#%%
+fig, ax = qs.stats.plot_uncertainties(model)
+fig
+
+#%%
+fig, ax = qs.stats.plot_uncertainties(model, x_axis=model.metrics[0], y_axis=model.metrics[1],
+                                      kind='kde-kde', center_kws={'fill': True})
+fig
