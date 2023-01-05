9f90c60b258c80180533579a0074e4e8b0b05ef8
Yalin Li <zoe.yalin.li@gmail.com>
2023-01-04 18:50:40 -0600

    add test for interface module, not finished

---
+++ tests/test_interface.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+'''
+EXPOsan: Exposition of sanitation and resource recovery systems
+
+This module is developed by:
+
+    Joy Zhang <joycheung1994@gmail.com>
+
+This module is under the University of Illinois/NCSA Open Source License.
+Please refer to https://github.com/QSD-Group/EXPOsan/blob/main/LICENSE.txt
+for license details.
+'''
+
+__all__ = ('test_interface',)
+
+def test_interface():
+    from numpy.testing import assert_allclose as ac
+    from exposan import interface as inter
+    inter.load()
+    sys = inter.interface_sys
+    sys.simulate(t_span=(0, 3))
+    
+    # # Below copied from bsm1
+    # assert sys.outs[0].isempty() == False
+    # ac(float(sys.outs[0].iconc['S_S']), 0.895, rtol=1e-2)
+    # ac(float(sys.outs[1].iconc['X_BH']), 4994.3, rtol=1e-2)
+    # ac(sys.outs[0].COD, 47.5, rtol=1e-2)
+    # ac(sys.outs[1].get_TSS(), 6377.9, rtol=1e-2)
+
+
+if __name__ == '__main__':
+    test_interface()
