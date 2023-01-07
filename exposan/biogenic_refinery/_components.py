f39146d706ee788df86e4dff3de6c2e55f25bc14
lane-to <91288288+lane-to@users.noreply.github.com>
2023-01-07 18:58:12 -0500

    Update _components.py

--- exposan/biogenic_refinery/_components.py
+++ exposan/biogenic_refinery/_components.py
@@ -19,15 +19,109 @@ for license details.
 
 from qsdsan import Chemical, Component, Components, set_thermo as qs_set_thermo
 from exposan.utils import add_V_from_rho
-from exposan.bwaise import create_components as create_bw_components
 
 __all__ = ('create_components', )
 
 def create_components(set_thermo=True):
-    bw_cmps = create_bw_components(set_thermo=False)
+
+    # NH3 through HAP are copied from Bwaise components    
+
+    NH3 = Component('NH3', measured_as='N',
+                    phase='l', particle_size='Soluble',
+                    degradability='Undegradable', organic=False)
+
+    NonNH3 = Component('NonNH3', formula='N', measured_as='N',
+                       phase='l', particle_size='Soluble',
+                       degradability='Undegradable', organic=False,
+                       description='Non-NH3 nitrogen')
+
+    P = Component('P', phase='l', particle_size='Soluble',
+                  degradability='Undegradable', organic=False)
+
+    K = Component('K', phase='l', particle_size='Soluble',
+                  degradability='Undegradable', organic=False)
+
+    Mg = Component('Mg', phase='l', particle_size='Soluble',
+                   degradability='Undegradable', organic=False)
+
+    Ca = Component('Ca', phase='l', particle_size='Soluble',
+                   degradability='Undegradable', organic=False)
+
+    H2O = Component('H2O', phase='l', particle_size='Soluble',
+                    degradability='Undegradable', organic=False)
+
+    OtherSS = Component('OtherSS', phase='l', particle_size='Soluble',
+                        degradability='Undegradable', organic=False,
+                        description='Unspecified soluble solids')
+
+    N2O = Component('N2O', phase='g', particle_size='Dissolved gas',
+                    degradability='Undegradable', organic=False)
+
+    CH4 = Component('CH4', phase='g', particle_size='Dissolved gas',
+                    degradability='Slowly', organic=True)
+
+    # Below three are for combustion reactions
+    O2 = Component('O2', phase='g', particle_size='Dissolved gas',
+                   degradability='Undegradable', organic=False)
+
+    N2 = Component('N2', phase='g', particle_size='Dissolved gas',
+                   degradability='Undegradable', organic=False)
+
+    CO2 = Component('CO2', phase='g', particle_size='Dissolved gas',
+                    degradability='Undegradable', organic=False)
+
+    P4O10 = Component.from_chemical('P4O10', Chemical('P4O10'),
+                                    phase='s', particle_size='Particulate',
+                                    degradability='Undegradable', organic=False)
+    # https://en.wikipedia.org/wiki/Phosphorus_pentoxide (accessed 2022-09-08)
+    add_V_from_rho(P4O10, 2390)
+    # The following will lead to an error as it won't be able to copy the V model
+    # P4O10 = Component('P4O10', phase='s', particle_size='Particulate',
+    #                   degradability='Undegradable', organic=False)
+
+    Tissue = Component('Tissue', MW=1, phase='s', particle_size='Particulate',
+                        degradability='Undegradable', organic=False,
+                        description='Tissue for toilet paper')
+    # 375 kg/m3 is the average of 250-500 for tissue from
+    # https://paperonweb.com/density.htm (accessed 2020-11-12)
+    add_V_from_rho(Tissue, 375)
+
+    WoodAsh = Component('WoodAsh', MW=1, phase='s', i_Mg=0.0224, i_Ca=0.3034,
+                        particle_size='Particulate', degradability='Undegradable',
+                        organic=False, description='Wood ash for desiccant')
+    add_V_from_rho(WoodAsh, 760)
+
+    for i in (Tissue, WoodAsh):
+        i.copy_models_from(Chemical('Glucose'), ('Cn', 'mu'))
+
+    Struvite = Component('Struvite', search_ID='MagnesiumAmmoniumPhosphate',
+                         formula='NH4MgPO4·H12O6',
+                         phase='s', particle_size='Particulate',
+                         degradability='Undegradable', organic=False)
+    # http://www.chemspider.com/Chemical-Structure.8396003.html (accessed 2020-11-19)
+    add_V_from_rho(Struvite, 1711)
+
+    HAP = Component('HAP', search_ID='Hydroxyapatite',
+                    phase='s', particle_size='Particulate',
+                    degradability='Undegradable', organic=False)
+    # Taking the average of 3.1-3.2 g/cm3 from
+    # https://pubchem.ncbi.nlm.nih.gov/compound/Hydroxyapatite (accessed 2020-11-19)
+    add_V_from_rho(HAP, 3150)
+
 
     C = Component('C', phase='l', particle_size='Soluble',
                   degradability='Undegradable', organic=False)
+    
+    # Ash, VolatileMatter, and FixedCarbon are components exclusive to the biochar stream in the biogenic refinery carbonizer base
+    
+    Ash = Component('Ash', phase='s', particle_size='Particulate', 
+                    degradability='Undegradable', organic=False)
+    
+    VolatileMatter = Component('VolatileMatter', phase='s', particle_size='Particulate', 
+                    degradability='Undegradable', organic=False)
+    
+    FixedCarbon = Component('FixedCarbon', phase='s', particle_size='Particulate', 
+                    degradability='Undegradable', organic=False)
 
     FilterBag = Component('FilterBag', search_ID='Poly(hexamethylene adipamide)',
                           formula='C12H20N2O2', phase='s', particle_size='Particulate',
@@ -116,8 +210,11 @@ def create_components(set_thermo=True):
         i.copy_models_from(Chemical('Glucose'), ('Cn', 'mu'))
 
     # Reuse components in the bwaise module for consistency
-    cmps = Components((*bw_cmps, C, FilterBag, Polymer, Resin, H2SO4, MgOH2, MgCO3,
-                        BambooWood, CoconutShell, CoconutHusk, RiceHusk, CornStover))
+    cmps = Components((NH3, NonNH3, P, K, Mg, Ca, H2O, OtherSS, N2O, CH4, O2, N2,
+                       CO2, P4O10, Tissue, WoodAsh, Struvite, HAP, C, Ash, 
+                       VolatileMatter, FixedCarbon, FilterBag, Polymer, Resin, 
+                       H2SO4, MgOH2, MgCO3, BambooWood, CoconutShell, CoconutHusk, 
+                       RiceHusk, CornStover))
 
     for i in cmps:
         for attr in ('HHV', 'LHV', 'Hf'):
