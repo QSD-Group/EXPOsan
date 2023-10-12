b7e2df2ae907debeee776921ef1d9914634f039c
Joy Zhang <joycheung1994@gmail.com>
2023-10-12 15:02:09 -0700

    Update test_metab.py

--- tests/test_metab.py
+++ tests/test_metab.py
@@ -17,28 +17,29 @@ def test_metab():
     from exposan.metab import create_system
     import qsdsan as qs, numpy as np
     
+    rtol=1e-3
     qs.PowerUtility.price = 0.0913
     UASB_M = create_system(n_stages=2, reactor_type='UASB', gas_extraction='M', tot_HRT=4)
     UASB_M.simulate(state_reset_hook='reset_cache', method='BDF', t_span=(0, 400))
     fs = UASB_M.flowsheet.stream
-    assert np.isclose(1 - fs.eff_dg.COD / fs.inf.COD, 0.9067142448236405)
-    assert np.isclose(UASB_M.TEA.annualized_NPV, -124172.15872927892)
-    assert np.isclose(UASB_M.LCA.total_impacts['GWP100'], 613761.7802056486)
+    assert np.isclose(1 - fs.eff_dg.COD / fs.inf.COD, 0.9067142448236405, rtol)
+    assert np.isclose(UASB_M.TEA.annualized_NPV, -124172.15872927892, rtol)
+    assert np.isclose(UASB_M.LCA.total_impacts['GWP100'], 613761.7802056486, rtol)
     
     FB_H = create_system(n_stages=2, reactor_type='FB', gas_extraction='H', tot_HRT=4)
     FB_H.simulate(state_reset_hook='reset_cache', method='BDF', t_span=(0, 400))
     fs = FB_H.flowsheet.stream
-    assert np.isclose(1 - fs.eff_dg.COD / fs.inf.COD, 0.8254341848530495)
-    assert np.isclose(FB_H.TEA.annualized_NPV, -34968.07019357201)
-    assert np.isclose(FB_H.LCA.total_impacts['GWP100'], 1004100.126558344)
+    assert np.isclose(1 - fs.eff_dg.COD / fs.inf.COD, 0.8254341848530495, rtol)
+    assert np.isclose(FB_H.TEA.annualized_NPV, -34968.07019357201, rtol)
+    assert np.isclose(FB_H.LCA.total_impacts['GWP100'], 1004100.126558344, rtol)
     
     PB_P = create_system(n_stages=2, reactor_type='PB', gas_extraction='P', tot_HRT=4)
     try: PB_P.simulate(state_reset_hook='reset_cache', method='BDF', t_span=(0, 400))
     except: PB_P.simulate(state_reset_hook='reset_cache', method='BDF', t_span=(0, 400))
     fs = PB_P.flowsheet.stream
-    assert np.isclose(1 - fs.eff_dg.COD / fs.inf.COD, 0.8261047568019022)
-    assert np.isclose(PB_P.TEA.annualized_NPV, -50509.157739631584)
-    assert np.isclose(PB_P.LCA.total_impacts['GWP100'], 178566.02455877443)
+    assert np.isclose(1 - fs.eff_dg.COD / fs.inf.COD, 0.8261047568019022, rtol)
+    assert np.isclose(PB_P.TEA.annualized_NPV, -50509.157739631584, rtol)
+    assert np.isclose(PB_P.LCA.total_impacts['GWP100'], 178566.02455877443, rtol)
     
 if __name__ == '__main__':
     test_metab()
