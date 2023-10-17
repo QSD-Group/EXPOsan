57c5d7c853467463a9415aab7c6ade0fb46879d6
Yalin <mailto.yalin.li@gmail.com>
2023-10-17 14:49:05 -0400

    fix bugs in model and add script for uncertainty/sensitivity analyses

--- exposan/pou_disinfection/analyses/uncertainty.py
+++ exposan/pou_disinfection/analyses/uncertainty.py
@@ -13,20 +13,32 @@ Please refer to https://github.com/QSD-Group/EXPOsan/blob/main/LICENSE.txt
 for license details.
 '''
 
-import os
+import os, numpy as np
 from exposan.pou_disinfection import (
     create_model,
     results_path,
     run_uncertainty,
     )
 
-sys_ID = 'A'
-water_source = 'GW'
-ppl = 1000
-N = 100
+water_sources = ['GW', 'SW']
+# water_sources = ['GW',]
 
-modelA = create_model(sys_ID, water_source=water_source, ppl=ppl)
+sys_IDs = ['A', 'B', 'C', 'D']
+# sys_IDs = ['A',]
+
+ppls = np.arange(500, 1100, 100)
+# ppls = [500, 1000]
+
+N = 1000
 
 if __name__ == '__main__':
-    path = os.path.join(results_path, f'{sys_ID}_{water_source}_ppl{ppl}_{N}.xlsx')
-    run_uncertainty(modelA, path=path, N=N)
+    for sys_ID in sys_IDs:
+        for water_source in water_sources:
+            for ppl in ppls:
+                model = create_model(
+                    model_ID=sys_ID,
+                    water_source=water_source,
+                    ppl=ppl,
+                    )
+                path = os.path.join(results_path, f'{sys_ID}_{water_source}_ppl{ppl}_{N}.xlsx')
+                run_uncertainty(model, path=path, N=N)
