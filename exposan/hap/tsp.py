28a11650a623f7c1488b7402923758b785732082
Joy Zhang <joycheung1994@gmail.com>
2024-02-08 09:35:17 -0800

    checkpoint

--- exposan/hap/tsp.py
+++ exposan/hap/tsp.py
@@ -19,7 +19,7 @@ from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 from math import ceil
 
-__all__ = ('Location', 'Locations',)
+__all__ = ('Location', 'Locations', 'SimpleCVRP')
 
 #%%
 class Location:
@@ -236,37 +236,6 @@ class SimpleCVRP:
     
     # def plot_routes(self):
 
-#%%
-
-def print_solution(data, manager, routing, solution):
-    """Prints solution on console."""
-    print(f"Objective: {solution.ObjectiveValue()}")
-    total_distance = 0
-    total_load = 0
-    for vehicle_id in range(data["num_vehicles"]):
-        index = routing.Start(vehicle_id)
-        plan_output = f"Route for vehicle {vehicle_id}:\n"
-        route_distance = 0
-        route_load = 0
-        while not routing.IsEnd(index):
-            node_index = manager.IndexToNode(index)
-            route_load += data["demands"][node_index]
-            plan_output += f" {node_index} Load({route_load}) -> "
-            previous_index = index
-            index = solution.Value(routing.NextVar(index))
-            route_distance += routing.GetArcCostForVehicle(
-                previous_index, index, vehicle_id
-            )
-        plan_output += f" {manager.IndexToNode(index)} Load({route_load})\n"
-        plan_output += f"Distance of the route: {route_distance}m\n"
-        plan_output += f"Load of the route: {route_load}\n"
-        print(plan_output)
-        total_distance += route_distance
-        total_load += route_load
-    print(f"Total distance of all routes: {total_distance}m")
-    print(f"Total load of all routes: {total_load}")
-
-        
 #%%
 n = 100
 seed = 52
