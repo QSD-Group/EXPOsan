4c54865f7b21674118b0c1fe55ab5d27624e5aa3
aaronpm3 <73093707+aaronpm3@users.noreply.github.com>
2024-12-02 22:09:24 -0600

    Create systems.py
    Created new branch for EnviroLoo project, with systems as the basis for TEA/LCA work

---
+++ exposan/enviroloo/systems.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 22:01:29 2024
+
+@author: Aaron Marszewski
+"""
+from qsdsan import (
+    Flowsheet, main_flowsheet,
+    WasteStream,
+    ImpactItem,
+    System, TEA, LCA,
+    )
