4d7a9e08f8cbc08190a7dd75a2a1c8009ba5250e
Joy Zhang <joycheung1994@gmail.com>
2025-02-08 16:34:55 -0800

    Update analysis_1.py

--- exposan/werf/analysis_1.py
+++ exposan/werf/analysis_1.py
@@ -4,10 +4,10 @@ Created on Fri Feb  7 08:41:06 2025
 
 @author: joy_c
 """
-import time as tm, pandas as pd, os
+import time as tm
 from exposan.werf import create_system, add_performance_metrics, results_path
 from exposan.werf.utils import plantwide_N_mass_flows
-from qsdsan import Model, sanunits as su
+from qsdsan import Model, sanunits as su, processes as pc
 from biosteam.evaluation._utils import var_columns
 
 import warnings
@@ -17,6 +17,12 @@ N_mass_dfs = {}
 metrics = {}
 pe = 108918     # person equivalent for 10 MGD WRRF
 
+pc.create_masm2d_cmps()
+EX = su.ExcretionmASM2d('EX')
+EX.simulate()
+urine, feces = EX.outs
+urine.scale(pe)
+        
 for ID in (
         'B1', 'B2', 'B3', 
         'C1', 'C2', 'C3', 
@@ -27,35 +33,40 @@ for ID in (
         'I1', 'I2', 'I3', 
         'N1', 'N2'
         ):
-    txt = f"System {ID}"
-    print(txt)
-    print("="*len(txt))
+    print(f"System {ID}")
+    print("="*30)
     sys = create_system(ID)
     fs = sys.flowsheet.stream
     mdl = Model(sys)
     add_performance_metrics(mdl)
 
-    start = tm.time()
-    print("Baseline start time: ", tm.strftime('%H:%M:%S', tm.localtime()))
-    sys.simulate(t_span=(0,300), method='BDF')
-    end = tm.time()
-    print('Duration: ', tm.strftime('%H:%M:%S', tm.gmtime(end-start)), '\n')
-    N_mass_dfs[ID] = plantwide_N_mass_flows(sys)
-    metrics[ID] = [m() for m in mdl.metrics]
-
-    if 'urine' not in fs:
-        EX = su.ExcretionmASM2d('EX', outs=('urine', 'feces'))
-        EX.simulate()
-        fs.urine.scale(pe)
-    fs.RWW.separate_out(fs.urine)
+    try:
+        start = tm.time()
+        print("Baseline start time: ", tm.strftime('%H:%M:%S', tm.localtime()))
+        sys.simulate(t_span=(0,300), method='BDF')
+        end = tm.time()
+        print('Duration: ', tm.strftime('%H:%M:%S', tm.gmtime(end-start)), '\n')
+        N_mass_dfs[ID] = plantwide_N_mass_flows(sys)
+        metrics[ID] = [m() for m in mdl.metrics]
+    except Exception as exc:
+        print(exc)        
+        
+    try:
+        fs.RWW.separate_out(urine)
+        start = tm.time()
+        print("With UD start time: ", tm.strftime('%H:%M:%S', tm.localtime()))
+        sys.simulate(state_reset_hook='reset_cache', t_span=(0,400), method='BDF')
+        end = tm.time()
+        print('Duration: ', tm.strftime('%H:%M:%S', tm.gmtime(end-start)), '\n')
+        N_mass_dfs[ID+'_UD'] = plantwide_N_mass_flows(sys)
+        metrics[ID+'_UD'] = [m() for m in mdl.metrics]
+    except Exception as exc:
+        print(exc)
     
-    start = tm.time()
-    print("With UD start time: ", tm.strftime('%H:%M:%S', tm.localtime()))
-    sys.simulate(state_reset_hook='reset_cache', t_span=(0,400), method='BDF')
-    end = tm.time()
-    print('Duration: ', tm.strftime('%H:%M:%S', tm.gmtime(end-start)), '\n')
-    N_mass_dfs[ID+'_UD'] = plantwide_N_mass_flows(sys)
-    metrics[ID+'_UD'] = [m() for m in mdl.metrics]
+    del sys, fs
+
+#%%
+import pandas as pd, os
 
 with pd.ExcelWriter(os.path.join(results_path, 'N_mass.xlsx')) as writer:
     for k, v in N_mass_dfs.items():
