92e1eb928ffd7dbcb29938c0583c57959abc610f
Joy Zhang <joycheung1994@gmail.com>
2025-03-18 13:09:17 -0700

    G2 met BNR requirements

--- exposan/werf/G2.py
+++ exposan/werf/G2.py
@@ -30,7 +30,7 @@ dfs = load_data(
     )
 asinit = dfs[ID]
 fcinit = asinit.iloc[-1].to_dict()
-default_fctss_init = [10, 12, 12, 40, 500, 500, 500, 5e3, 1e4, 1.3e4]
+default_fctss_init = [8, 8, 9, 20, 270, 270, 270, 270, 270, 7400]
 aedinit = dfs['AED'].loc[ID].to_dict()
 
 MGD2cmd = 3785.412
@@ -47,7 +47,7 @@ def create_g2_system(flowsheet=None, default_init_conds=True):
         COD=358, NH4_N=25.91, PO4_P=5,
         fr_SI=0.05, fr_SF=0.16, fr_SA=0.024, fr_XI=0.2,
         )
-    carb = WasteStream('carbon', T=Temp, units='kg/hr', S_A=85)
+    carb = WasteStream('carbon', T=Temp, units='kg/hr', S_A=210)    # how much it takes to reduce eff TP to <= 2mg/L
     PC = su.PrimaryClarifier(
         'PC', ins=[rww, 'reject'], 
         outs=('PE', 'PS'),
@@ -64,7 +64,8 @@ def create_g2_system(flowsheet=None, default_init_conds=True):
     
     n_zones = 6
     V_tot = 4.7 * MGD2cmd
-    fr_V = [0.014, 0.13, 0.148, 0.148, 0.28, 0.28]
+    # fr_V = [0.014, 0.13, 0.148, 0.148, 0.28, 0.28]
+    fr_V = [0.044, 0.10, 0.148, 0.148, 0.28, 0.28]  # increasing 1st anoxic zone volume helps reduce NOx- entering anaerobic zone
     
     # gstrip = True
     # an_kwargs = dict(aeration=None, DO_ID='S_O2', suspended_growth_model=asm, gas_stripping=gstrip)
@@ -89,8 +90,9 @@ def create_g2_system(flowsheet=None, default_init_conds=True):
             [1,0,0,0,0,0],          # RAS
             [1,0,0,0,0,0],          # carb
             ],
-        internal_recycles=[(5,2,40*MGD2cmd)], DO_ID='S_O2',
-        kLa=[0, 0, 0, 0, 180, 70], 
+        # internal_recycles=[(5,2,40*MGD2cmd)], 
+        internal_recycles=[(5,2,30*MGD2cmd)], 
+        DO_ID='S_O2', kLa=[0, 0, 0, 0, 180, 70], 
         DO_setpoints=[0, 0, 0, 0, 2.0, 2.0],
         suspended_growth_model=asm,
         gas_stripping=True
@@ -99,7 +101,8 @@ def create_g2_system(flowsheet=None, default_init_conds=True):
     FC = su.FlatBottomCircularClarifier(
         'FC', ins=ASR-0, outs=['SE', 1-ASR, 'WAS'],
         # 'FC', ins=O6-1, outs=['SE', 1-A1, 'WAS'],
-        underflow=0.4*10*MGD2cmd, wastage=0.136*MGD2cmd,
+        # underflow=0.4*10*MGD2cmd, wastage=0.136*MGD2cmd,
+        underflow=0.4*10*MGD2cmd, wastage=0.28*MGD2cmd,     # shorter SRT seems better for EBPR
         surface_area=1579.352, height=3.6576, N_layer=10, feed_layer=5,
         X_threshold=3000, v_max=410, v_max_practical=274,
         rh=4e-4, rp=0.1, fns=0.01, 
@@ -108,24 +111,23 @@ def create_g2_system(flowsheet=None, default_init_conds=True):
     
     MT = su.IdealClarifier(
         'MT', FC-2, outs=['', 'thickened_WAS'],
-        sludge_flow_rate=0.023*MGD2cmd,
+        sludge_flow_rate=0.0395*MGD2cmd,
         solids_removal_efficiency=0.95
         )
-    M1 = su.Mixer('M1', ins=[GT-1, MT-1])
     
     AED = su.AerobicDigester(
-        'AED', ins=M1-0, outs='digestate',
+        'AED', ins=[GT-1, MT-1], outs='digestate',
         V_max=2.4*MGD2cmd, activated_sludge_model=asm,
         aeration=1.0, DO_ID='S_O2', gas_stripping=True)
 
     DW = su.IdealClarifier(
         'DW', AED-0, outs=('', 'cake'),
-        sludge_flow_rate=3e-3*MGD2cmd,
+        sludge_flow_rate=0.0046*MGD2cmd,  # aim for 17%
         solids_removal_efficiency=0.9
         )
-    M2 = su.Mixer('M2', ins=[MT-0, DW-0])
+    MX = su.Mixer('MX', ins=[GT-0, MT-0, DW-0])
     
-    HD = su.HydraulicDelay('HD', ins=M2-0, outs=1-PC)
+    HD = su.HydraulicDelay('HD', ins=MX-0, outs=1-PC)
     
     if default_init_conds:
         # asdct = asinit.to_dict('index')
@@ -142,7 +144,7 @@ def create_g2_system(flowsheet=None, default_init_conds=True):
         # path=(PC, GT, S1, A1, A2, A3, A4, O5, O6, FC, 
         #       MT, M1, AED, DW, M2, HD),
         # recycle=(O6-0, FC-1, HD-0)
-        path=(PC, GT, ASR, FC, MT, M1, AED, DW, M2, HD),
+        path=(PC, GT, ASR, FC, MT, AED, DW, MX, HD),
         recycle=(FC-1, HD-0)
         )
     sys.set_dynamic_tracker(FC-0, AED)
