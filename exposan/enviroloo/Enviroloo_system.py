9a2d179faae6235bd147188556a1303897a5ff26
rishabhpuri34 <rp34@illinois.edu>
2025-03-25 16:04:47 -0500

    added components to system file

--- exposan/enviroloo/Enviroloo_system.py
+++ exposan/enviroloo/Enviroloo_system.py
@@ -20,11 +20,21 @@ from qsdsan import (
     LCA, TEA, System,
     )
 from qsdsan.sanunits import Trucking
+from chaospy import distributions as shape
+from qsdsan.utils import clear_lca_registries
+# from qsdsan.utils import load_components, set_thermo
 
+
+import qsdsan as qs
+import os
+from qsdsan import Component, Components, set_thermo as qs_set_thermo
+from exposan.utils import add_V_from_rho
+from exposan.bwaise import create_components as create_bw_components
+import numpy as np
+from qsdsan import processes as pc, sanunits as su, Model as mod
 from chaospy import distributions as shape
 
-from qsdsan.utils import clear_lca_registries
-from qsdsan.utils import load_components, set_thermo
+
 from exposan.utils import add_fugitive_items
 from exposan.enviroloo import _units as elu
 from exposan.enviroloo import (
@@ -124,7 +134,80 @@ def update_carbon_COD_ratio(sys):
         if hasattr(u, 'carbon_COD_ratio'): u.carbon_COD_ratio = carbon_COD_ratio
 
 # %% Create EnviroLoo Clear system
+
+def create_components(set_thermo = True
+                      #adjust_MW_to_measured_as=False
+                      ):
+    # bw_cmps = create_bw_components(set_thermo=False)
+    masm2d_cmps = pc.create_masm2d_cmps(set_thermo=True)
+
+    # C = Component('C', phase='l', particle_size='Soluble', degradability='Undegradable', organic=False)
+    
+    # SolubleCH4 = Component('SolubleCH4', search_ID='CH4', phase='l', particle_size='Soluble', degradability='Slowly', organic=True)
+
+    # PAC = Component('PAC', search_ID='10124-27-3', phase='s', particle_size='Particulate', degradability='Slowly', organic=False)
+    # add_V_from_rho(PAC, rho=2800)
+                    
+    # # Glucose = Component('Glucose', search_ID='50-99-7', phase='s', particle_size='Particulate', degradability='Readily', organic=False)
+    # # add_V_from_rho(Glucose, rho=1560)
+
+    # Glucose = Component('Glucose', search_ID='50-99-7', phase='l', particle_size='Soluble', degradability='Readily', organic=True)
+    # add_V_from_rho(Glucose, rho=1560)
+          
+    # O3 = Component('O3', search_ID='10028-15-6', phase='g', particle_size='Dissolved gas', degradability='Readily', organic=False)
+          
+          
+    # NaOH = Component('NaOH', search_ID='1310-73-2', phase='s', particle_size='Particulate', degradability='Readily', organic=False)
+    # add_V_from_rho(NaOH, rho=2130)
+          
+    # NaClO = Component('NaClO', search_ID='7681-52-9', phase='s', particle_size='Particulate', degradability='Readily', organic=False)
+    # add_V_from_rho(NaClO, rho=1250)
+    
+    # # NO3 = Component('NO3', measured_as = 'N', phase='l', particle_size='Soluble', degradability='Undegradable', organic=False)
+    # # add_V_from_rho(NO3, rho=1.15) # need check
+          
+    # #NH3_l = Component('NH3_l', measured_as = 'N', phase='l', particle_size='Soluble', degradability='Undegradable', organic=False)
+          
+    # #NonNH3 = Component('NonNH3', formula = 'N', measured_as = 'N', phase='l', particle_size='Soluble', degradability='Undegradable', organic=False, description='Non-NH3');
+          
+    # air = Component('air', search_ID='17778-88-0', phase='g', particle_size='Dissolved gas',
+    #                 degradability='Readily', organic=False)
+    #                 # 1.204 kg/m3, cited from https://en.wikipedia.org/wiki/Density_of_air#:~:text=Air%20density%2C%20like%20air%20pressure,International%20Standard%20Atmosphere%20(ISA).
+    # add_V_from_rho(air, rho=1.204)
+
+    #allowed_values = {
+    #'particle_size': ('Dissolved gas', 'Soluble', 'Colloidal', 'Particulate'),
+    #'degradability': ('Readily', 'Slowly', 'Undegradable'),
+    #'organic': (True, False)}
+          
+    cmps = Components((masm2d_cmps 
+                       # C, SolubleCH4, 
+                       # #H2O, CO2, CH4, N2O, NH3
+                       # Glucose, O3, air, PAC, NaOH, NaClO
+                       ))
+    
+    # for i in cmps:
+    #     for attr in ('HHV', 'LHV', 'Hf'):
+    #         if getattr(i, attr) is None: setattr(i, attr, 0)
+
+    cmps.compile()
+
+    # cmps.set_alias('H2O', 'Water')
+    # #cmps.set_alias('CO2', 'Carbon Dioxide')
+    # cmps.set_alias('CH4', 'Methane')
+    # if set_thermo: qs_set_thermo(cmps)
+
+    return cmps
+
+
+
 def create_systemEL(flowsheet = None):
+    
+    # Components and stream
+    cmps = create_components()
+    qs.set_thermo(cmps)
+    thermo_masm2d = qs.get_thermo()
+    
     flowsheet = flowsheet or main_flowsheet
     streamEL = flowsheet.stream
     batch_create_streams('EL')
--- exposan/enviroloo/_components.py
+++ exposan/enviroloo/_components.py
@@ -13,69 +13,73 @@ import os
 from qsdsan import Component, Components, set_thermo as qs_set_thermo
 from exposan.utils import add_V_from_rho
 from exposan.bwaise import create_components as create_bw_components
-from qsdsan import processes as pc, sanunits as su
+import numpy as np
+from qsdsan import processes as pc, sanunits as su, Model as mod
+from chaospy import distributions as shape
 
 __all__ = ('create_components', )
 
 ## Pre-define the involving components in the toilet system of concern such as Enviroloo Clear
-def create_components(set_thermo = True, 
+def create_components(set_thermo = True
                       #adjust_MW_to_measured_as=False
                       ):
     # bw_cmps = create_bw_components(set_thermo=False)
-    masm2d_cmps = pc.create_masm2d_cmps(set_thermo=False)
+    masm2d_cmps = pc.create_masm2d_cmps(set_thermo=True)
 
-    C = Component('C', phase='l', particle_size='Soluble', degradability='Undegradable', organic=False)
+    # C = Component('C', phase='l', particle_size='Soluble', degradability='Undegradable', organic=False)
     
-    SolubleCH4 = Component('SolubleCH4', search_ID='CH4', phase='l', particle_size='Soluble', degradability='Slowly', organic=True)
+    # SolubleCH4 = Component('SolubleCH4', search_ID='CH4', phase='l', particle_size='Soluble', degradability='Slowly', organic=True)
 
-    PAC = Component('PAC', search_ID='10124-27-3', phase='s', particle_size='Particulate', degradability='Slowly', organic=False)
-    add_V_from_rho(PAC, rho=2800)
+    # PAC = Component('PAC', search_ID='10124-27-3', phase='s', particle_size='Particulate', degradability='Slowly', organic=False)
+    # add_V_from_rho(PAC, rho=2800)
                     
-    # Glucose = Component('Glucose', search_ID='50-99-7', phase='s', particle_size='Particulate', degradability='Readily', organic=False)
-    # add_V_from_rho(Glucose, rho=1560)
+    # # Glucose = Component('Glucose', search_ID='50-99-7', phase='s', particle_size='Particulate', degradability='Readily', organic=False)
+    # # add_V_from_rho(Glucose, rho=1560)
 
-    Glucose = Component('Glucose', search_ID='50-99-7', phase='l', particle_size='Soluble', degradability='Readily', organic=True)
-    add_V_from_rho(Glucose, rho=1560)
+    # Glucose = Component('Glucose', search_ID='50-99-7', phase='l', particle_size='Soluble', degradability='Readily', organic=True)
+    # add_V_from_rho(Glucose, rho=1560)
           
-    O3 = Component('O3', search_ID='10028-15-6', phase='g', particle_size='Dissolved gas', degradability='Readily', organic=False)
+    # O3 = Component('O3', search_ID='10028-15-6', phase='g', particle_size='Dissolved gas', degradability='Readily', organic=False)
           
           
-    NaOH = Component('NaOH', search_ID='1310-73-2', phase='s', particle_size='Particulate', degradability='Readily', organic=False)
-    add_V_from_rho(NaOH, rho=2130)
+    # NaOH = Component('NaOH', search_ID='1310-73-2', phase='s', particle_size='Particulate', degradability='Readily', organic=False)
+    # add_V_from_rho(NaOH, rho=2130)
           
-    NaClO = Component('NaClO', search_ID='7681-52-9', phase='s', particle_size='Particulate', degradability='Readily', organic=False)
-    add_V_from_rho(NaClO, rho=1250)
+    # NaClO = Component('NaClO', search_ID='7681-52-9', phase='s', particle_size='Particulate', degradability='Readily', organic=False)
+    # add_V_from_rho(NaClO, rho=1250)
     
-    # NO3 = Component('NO3', measured_as = 'N', phase='l', particle_size='Soluble', degradability='Undegradable', organic=False)
-    # add_V_from_rho(NO3, rho=1.15) # need check
+    # # NO3 = Component('NO3', measured_as = 'N', phase='l', particle_size='Soluble', degradability='Undegradable', organic=False)
+    # # add_V_from_rho(NO3, rho=1.15) # need check
           
-    #NH3_l = Component('NH3_l', measured_as = 'N', phase='l', particle_size='Soluble', degradability='Undegradable', organic=False)
+    # #NH3_l = Component('NH3_l', measured_as = 'N', phase='l', particle_size='Soluble', degradability='Undegradable', organic=False)
           
-    #NonNH3 = Component('NonNH3', formula = 'N', measured_as = 'N', phase='l', particle_size='Soluble', degradability='Undegradable', organic=False, description='Non-NH3');
+    # #NonNH3 = Component('NonNH3', formula = 'N', measured_as = 'N', phase='l', particle_size='Soluble', degradability='Undegradable', organic=False, description='Non-NH3');
           
-    air = Component('air', search_ID='17778-88-0', phase='g', particle_size='Dissolved gas',
-                    degradability='Readily', organic=False)
-                    # 1.204 kg/m3, cited from https://en.wikipedia.org/wiki/Density_of_air#:~:text=Air%20density%2C%20like%20air%20pressure,International%20Standard%20Atmosphere%20(ISA).
-    add_V_from_rho(air, rho=1.204)
+    # air = Component('air', search_ID='17778-88-0', phase='g', particle_size='Dissolved gas',
+    #                 degradability='Readily', organic=False)
+    #                 # 1.204 kg/m3, cited from https://en.wikipedia.org/wiki/Density_of_air#:~:text=Air%20density%2C%20like%20air%20pressure,International%20Standard%20Atmosphere%20(ISA).
+    # add_V_from_rho(air, rho=1.204)
 
     #allowed_values = {
     #'particle_size': ('Dissolved gas', 'Soluble', 'Colloidal', 'Particulate'),
     #'degradability': ('Readily', 'Slowly', 'Undegradable'),
     #'organic': (True, False)}
           
-    cmps = Components((*masm2d_cmps, C, SolubleCH4, 
-                       #H2O, CO2, CH4, N2O, NH3
-                       Glucose, O3, air, PAC, NaOH, NaClO))
+    cmps = Components((*masm2d_cmps, 
+                       # C, SolubleCH4, 
+                       # #H2O, CO2, CH4, N2O, NH3
+                       # Glucose, O3, air, PAC, NaOH, NaClO
+                       ))
     
-    for i in cmps:
-        for attr in ('HHV', 'LHV', 'Hf'):
-            if getattr(i, attr) is None: setattr(i, attr, 0)
+    # for i in cmps:
+    #     for attr in ('HHV', 'LHV', 'Hf'):
+    #         if getattr(i, attr) is None: setattr(i, attr, 0)
 
     cmps.compile()
 
-    cmps.set_alias('H2O', 'Water')
-    #cmps.set_alias('CO2', 'Carbon Dioxide')
-    cmps.set_alias('CH4', 'Methane')
+    # cmps.set_alias('H2O', 'Water')
+    # #cmps.set_alias('CO2', 'Carbon Dioxide')
+    # cmps.set_alias('CH4', 'Methane')
     if set_thermo: qs_set_thermo(cmps)
 
     return cmps
