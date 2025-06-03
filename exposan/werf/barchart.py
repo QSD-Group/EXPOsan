e5a1229d0455cae2318bf79dcec7f2aa0bd90704
Joy Zhang <joycheung1994@gmail.com>
2025-06-03 10:26:07 -0700

    opex plotting functions

---
+++ exposan/werf/barchart.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+EXPOsan: Exposition of sanitation and resource recovery systems
+
+This module is developed by:
+        
+    Joy Zhang <joycheung1994@gmail.com>
+
+This module is under the University of Illinois/NCSA Open Source License.
+Please refer to https://github.com/QSD-Group/EXPOsan/blob/main/LICENSE.txt
+for license details.
+"""
+
+import pandas as pd, numpy as np, matplotlib.pyplot as plt
+from qsdsan.utils import load_data, ospath, palettes
+from exposan.werf import results_path, figures_path
+
+Guest = palettes['Guest']
+
+b = Guest.blue.HEX
+g = Guest.green.HEX
+r = Guest.red.HEX
+o = Guest.orange.HEX
+y = Guest.yellow.HEX
+p = Guest.purple.HEX
+db = Guest.dblue.HEX
+
+#%%
+plt.rcParams['font.sans-serif'] = 'Arial'
+plt.rcParams["figure.autolayout"] = False
+plt.rcParams['ytick.minor.visible'] = True
+
+categories = ['Aeration', 'Pumping', 'Mechanical mixing', 
+              'External carbon', 'Coagulant', 'Lime stabilization', 
+              'Sludge disposal']
+patch_dct = {
+    'Aeration energy': (b, ''),
+    'Pumping energy': (p, '-----'),
+    'Mixing energy': (y, r'\\\\\\'),
+    'Coagulant': (o, '/////'),
+    'External carbon': (r, ''),
+    'Lime stabilization': (g, '|||||'),
+    'Sludge disposal': (db, ''),
+    }
+
+configs=('B1', 'B2', 'B3', 'C1', 'C2', 'C3', 'E2', 'E2P', 'F1', 
+        'G1', 'G2', 'G3', 'H1', 'I1', 'I2', 'I3', 'N1', 'N2')
+data_handles = {
+    'Baseline': ('baseline_opt_performance', 'combined'),   # file name, sheet name
+    'UD10': ('UD_opt_performance', '10'),
+    # 'UD30': ('UD_opt_performance', '30'),
+    'UD100': ('UD_opt_performance', '100'),
+    # 'HA': ('HA_opt_performance', 0),
+    # 'ECS': ('ECS_opt_performance', 0)
+    }
+
+#%%
+def compile_opex():
+    opex = []
+    keys = []
+    for k, v in data_handles.items():
+        file, sheet = v
+        df = load_data(
+            ospath.join(results_path, f'{file}.xlsx'), sheet=sheet,
+            header=[0,1], skiprows=[2,]
+            )
+        df = df.loc[:,['OPEX']]
+        opex.append(df)
+        keys.append(k)
+    
+    opex = pd.concat(opex, keys=keys)
+    opex.columns = [i[1].split(' cost')[0] for i in opex.columns]
+    opex['Lime stabilization'] = opex.loc[:,['Lime', 'Lime stablization energy']].sum(axis=1)
+    opex.drop(columns=['Lime stablization energy', 'Total OPEX [USD/d]'], inplace=True)
+    return opex
+
+#%%
+def stacked_bar(opex=None, save_as=''):
+    if opex is None:
+        opex = compile_opex()
+    fig, axes = plt.subplots(ncols=len(configs), sharey=True, figsize=(18, 6))
+    handles = []
+    for ID, ax in zip(configs, axes):
+        df = opex.loc[(slice(None), ID),:]
+        df.index = df.index.droplevel(1)
+        x = np.arange(df.shape[0])
+        y_offset = np.zeros(len(x))
+        for cat, style in patch_dct.items():
+            c, hat = style
+            y = df.loc[:,cat].to_numpy()
+            patch = ax.bar(x, y, bottom=y_offset, width=0.65, 
+                           color=c, hatch=hat, hatch_linewidth=0.5,
+                           tick_label=df.index.values)
+            if ID == 'B1': handles.append(patch)
+            y_offset += y
+        ax.set_title(ID, fontsize=14, fontweight='bold')
+        ax.set_xlim((-0.75, df.shape[0]-0.25))
+        if ID == 'B1': 
+            ax.set_ylabel('$\mathbf{OPEX}$ [USD·day${^{-1}}$]', fontname='Arial', fontsize=14)
+        ax.tick_params(axis='y', which='major', direction='inout', length=8, labelsize=12)
+        ax.tick_params(axis='y', which='minor', direction='inout', length=5)
+        ax.tick_params(axis='x', which='major', direction='out', length=4, labelsize=11, labelrotation=90)
+        if ID == 'N2':
+            ax2y = ax.secondary_yaxis('right')
+            ax2y.tick_params(axis='y', which='major', direction='in', length=4)
+            ax2y.tick_params(axis='y', which='minor', direction='in', length=2.5)
+            ax2y.yaxis.set_major_formatter(plt.NullFormatter())
+    
+    fig.legend(handles=handles, labels=categories, reverse=True,
+               bbox_to_anchor=(0.135, 0.85), loc='upper left', 
+               framealpha=1, fontsize=12)
+    fig.subplots_adjust(wspace=0, bottom=0.15)
+    if save_as:
+        fig.savefig(ospath.join(figures_path, save_as),
+                    dpi=300, transparent=True)
+    else:
+        return fig, axes
+
+#%%
+if __name__ == '__main__':
+    opex = compile_opex()
+    stacked_bar(opex, save_as='opex.png')
--- exposan/werf/opt_UD.py
+++ exposan/werf/opt_UD.py
@@ -29,8 +29,8 @@ def display_metrics(model):
 MGD2cmd = 3785.412
 
 # pct = 0.1
-# pct = 0.3
-pct = 1.0
+pct = 0.3
+# pct = 1.0
 pe = 108918     # person equivalent for 10 MGD WRRF
 try: EX = su.ExcretionmASM2d('EX')
 except: 
@@ -84,9 +84,9 @@ else:
     thickened = s.thickened_sludge
     thickener = u.GT
     
-# thickener.sludge_flow_rate, u.DW.sludge_flow_rate = opt_underflows[ID]
-thickener.sludge_flow_rate, u.DW.sludge_flow_rate = (154.10, 13.52)
-# load_state(sys, folder='steady_states/baseline_unopt')
+thickener.sludge_flow_rate, u.DW.sludge_flow_rate = opt_underflows[ID]
+# thickener.sludge_flow_rate, u.DW.sludge_flow_rate = (154.10, 13.52)
+load_state(sys, folder='steady_states/baseline_unopt')
 # load_state(sys, folder='steady_states/UD100_opt')
     
 #%%
@@ -94,25 +94,26 @@ thickener.sludge_flow_rate, u.DW.sludge_flow_rate = (154.10, 13.52)
 # u.ASR.DO_setpoints += 1
 # u.ASR.DO_setpoints[:] = [0,0,0,0,1,1]
 # u.ASR.DO_setpoints[:] = [0.5,0,2,2,0,1]
-# u.ASR.DO_setpoints[:] = [0,0,2,2,0,2]
+# u.ASR.DO_setpoints[:] = [0,0,1,1,0,1]
 u.ASR.DO_setpoints[:] = [0,0,1,1,0]
 # Vs = [0.63, 1.5, 2.0, 2.0, 2.3, 0.21] # MG
 # Vs = [0.84, 1.5, 2.0, 2.0, 2.1, 0.2]
 # Vs = [0.99, 1.35, 2.0, 2.0, 2.1, 0.2]
-# Vs = [0.3, 1.5, 2.03, 2.1, 2.3, 0.41]
+# Vs = [0.33, 1.5, 2.0, 2.1, 2.3, 0.41]
 # u.ASR.V_tanks[:] = [v * MGD2cmd for v in Vs]
 V_tot = 2.61 * MGD2cmd
 # fr_V = [0.12, 0.18, 0.24, 0.24, 0.18, 0.04]
-fr_V = [0.16, 0.16, 0.24, 0.24, 0.17, 0.03]     # larger anaerobic zone seems better for EBPR
+fr_V = [0.18, 0.14, 0.24, 0.24, 0.16, 0.04]
+# fr_V = [0.16, 0.16, 0.24, 0.24, 0.17, 0.03]     # larger anaerobic zone seems better for EBPR
 u.ASR.V_tanks[:] = [v * V_tot for v in fr_V[:-1]]
-# u.ASR.internal_recycles[0] = (3,1,20*MGD2cmd)
+# u.ASR.internal_recycles[0] = (3,1,40*MGD2cmd)
 u.ASR._ODE = None
 
 Q_ras = 2 * 10 * MGD2cmd
 Q_was = 0.2 * MGD2cmd
 u.MBR.pumped_flow = Q_ras + Q_was
 u.S1.split = Q_ras / (Q_ras + Q_was)
-#!!! must reset cache
+# !!! must reset cache
 
 u.MBR.V_max = fr_V[-1] * V_tot
 u.MBR.aeration = 1.0
@@ -122,11 +123,11 @@ u.MBR._ODE = None
 #     unit.aeration = 1.0
 #     unit._ODE = None
 
-s.carbon.imass['S_A'] = 0
+s.carbon.imass['S_A'] = 70
 s.carbon._init_state()
-# u.MD.metal_dosage = 6
+# u.MD.metal_dosage = 10
 # u.MD._AE = None
-# u.FC.underflow = 0.5 * 10 * MGD2cmd
+# u.FC.underflow = 0.67 * 10 * MGD2cmd
 # u.FC.wastage = 0.1 * MGD2cmd
 # u.FC._ODE = None
 
@@ -172,4 +173,4 @@ print(f'MLSS ~ {np.mean(mlss):.0f} mg/L')
 #%%
 df = display_metrics(mdl)
 df.T.to_clipboard()
-cache_state(sys, 'steady_states/UD100_opt')
+cache_state(sys, 'steady_states/UD10_opt')
