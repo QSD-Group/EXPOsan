d5d8d5e4a7fe9a2cd4b2d7504e783d83dcad6698
Yalin Li <mailto.yalin.li@gmail.com>
2025-06-12 17:02:30 -0400

    update analysis

--- exposan/phosphorus/analysis.py
+++ exposan/phosphorus/analysis.py
@@ -13,6 +13,69 @@ Please refer to https://github.com/QSD-Group/EXPOsan/blob/main/LICENSE.txt
 for license details.
 '''
 
+'''
+Simulation Findings:
+    
+    - Changing the input step did not change decrease simulation time,
+    either of the results can be used as the baseline for the given concentration.
+    
+    original input vs. t_step=1 input, inf=15 mg/L, Q=50 m3/d, t=25 d, 5-6 min
+    (ecorecover_results_15mgL_25d_50m3d_2025-6-12-10-3-19
+     ecorecover_results_15mgL_25d_50m3d_2025-6-12-10-9-22)
+    
+    original input vs. t=0/1000 input, inf=13 mg/L, Q=50 m3/d, t=5 d, ~1 min
+    (ecorecover_results_13mgL_5d_50m3d_2025-6-12-10-24-34
+    ecorecover_results_13mgL_5d_50m3d_2025-6-12-10-30-52)
+    
+    Now only has t=0 and t=1000 d influent data after 2025-6-12-10-56-00
+    
+    - Cycling occured for 100-d simulations, though biomass appears to be recovering,
+    the general trend is still to decrease.
+    (ecorecover_results_15mgL_100d_50m3d_2025-6-12-10-41-47, took 21 min).
+    
+    - Simulating 13 mg/L for 5 days took around 1 min, 100 days took 20 min.
+    (ecorecover_results_13mgL_5d_50m3d_2025-6-12-10-30-52
+     ecorecover_results_13mgL_100d_50m3d_2025-6-12-11-17-43)
+    
+    Also observed cycling. 5-day results are identical to the first 5 days of 100-day results.
+    
+    - Doubling V_mix leads to earlier effluent P increase and higher effluent P
+    compared to the original settings,
+    but the simulation time almost halved <3 min vs. 5-6 min).
+    (ecorecover_results_15mgL_25d_50m3d_2025-6-12-11-27-11
+     ecorecover_results_15mgL_25d_50m3d_2025-6-12-10-9-22)
+    
+    - Doubling V_pbr greatly improves treatment performance,
+    but leads to longer simulation time (8 min vs. 5-6 min).
+    (ecorecover_results_15mgL_25d_50m3d_2025-6-12-11-54-4
+     ecorecover_results_15mgL_25d_50m3d_2025-6-12-10-9-22)
+    
+    Running for 100 days with doubled V_pbr more than doubled the simulation time,
+    still observed biomass cycling, but the trend is going up instead of down,
+    significant increase in effluent P still observed around day 73-76 and day 95 towards the end.
+    (ecorecover_results_15mgL_100d_50m3d_2025-6-12-12-42-52
+     ecorecover_results_15mgL_25d_50m3d_2025-6-12-10-9-22)
+    
+    - Setting V_pbr based on influent P led to much better treatment performance.
+    (ecorecover_results_15mgL_25d_50m3d_2025-6-12-14-12-9
+     ecorecover_results_15mgL_100d_50m3d_2025-6-12-15-7-38)
+    
+    25-d simulation took 9-10 min, 100-d simulation took 59 min.
+    
+    - When V_pbr was adjusted based on the influent P,
+    changing Q did not affect results in a significant way
+
+    (ecorecover_results_15mgL_5d_1000m3d_2025-6-12-14-51-6
+     ecorecover_results_15mgL_5d_50m3d_2025-6-12-14-57-32)
+     
+     The simulation time nearly doubled for 1000 m3/d (90 min vs. 50 min for 50 m3/d)
+     ecorecover_results_15mgL_100d_50m3d_2025-6-12-15-7-38
+     ecorecover_results_15mgL_100d_1000m3d_2025-6-12-16-56-23)
+    
+    - Tried to adjust initial condition setting, but does not appear to be working, gave up.
+    
+'''
+
 # Test if the ecorecovery module works
 # from exposan import pm2_ecorecover
 # pm2_ecorecover.load()
@@ -24,7 +87,8 @@ for license details.
 
 # %%
 
-import os, numpy as np, qsdsan as qs
+import os, datetime, numpy as np, qsdsan as qs
+from qsdsan.utils import time_printer
 from exposan import pm2_ecorecover as pm2
 from exposan.phosphorus import (
     data_path,
@@ -32,28 +96,75 @@ from exposan.phosphorus import (
     results_path,
     )
 
-Temp = 286.08          # temperature [K]
-f_dct = None
+V_pbr_default = pm2.V_pbr
+V_pbr_indv_default = V_pbr_default / 20
+Q_default = 449.06
+all_infs = range(3, 16) # inf=3-15 mg/L
 
-S_P_dct = { # steady state effluent P in mg/L, when Q=1000 m3/d
-    '3': 0.103786314929568,
-    '4': 0.757374963832081,
-    '5': 1.38383912772804,
-    '6': 2.03682213914134,
-    '7': 2.68256462847412,
-    '8': 3.32964385968618,
-    '9': 3.9775093368395,
-    '10': 4.68,
-    '11': 5.33,
-    '12': 5.98,
-    '13': 6.63,
-    '14': 7.28,
-    '15': 7.93,
+# Initial conditions in EcoRecover
+default_init_conds = {
+        'X_CHL':2.53, # Chlorophyll content of cells, g Chl/m3
+        'X_ALG':505.97, # Carbon-accumulating mixotrophic organisms, g COD/m3
+        'X_PG':22.99, # Stored carbohydrates, g COD/m3
+        'X_TAG':93.78, # Stored lipids, g COD/m3
+        'S_CO2':30.0, # Soluble carbon dioxide, g CO2/m3
+        'S_A':5.0, # Extracellular dissolved organic carbon (acetate), g COD/m3
+        'S_G':5.0, # Extracellular dissolved organic carbon (glucose), g COD/m3
+        'S_O2':5.0, # Dissolved oxygen, g O2/m3
+        'S_NH':35.80, # Dissolved ammonium, g N/m3
+        'S_NO':0.7, # Dissolved nitrate/nitrite, g N/m3
+        'S_P':0.36, # Dissolved phosphorus, g P/m3
+        'X_N_ALG':3.23, # Stored nitrogen in microalgal cell, g N/m3
+        'X_P_ALG':0.19, # Stored phosphorus in microalgal cell, g P/m3
     }
 
+# Set initial conditions based on BioWin effluent
+init_cond_dct = {}
+for mgL in all_infs:
+    path = os.path.join(data_path, f'biowin_effluent_ph{mgL}.tsv')
+    df = qs.utils.load_data(path)
+    init_cond_dct[str(mgL)] = init_conds = default_init_conds.copy()
+    init_conds['S_NH'] = df.S_NH[0]
+    init_conds['S_NO'] = df.S_NO[0]
+    init_conds['S_P'] = df.S_P[0]
 
-def create_flowsheets(f_dct=f_dct, inf_range=range(3, 16), Q=50): # inf=3-15 mg/L
-    f_dct = f_dct or dict.fromkeys(f'f{n}' for n in inf_range) 
+# BioWin effluent P
+# S_P_dct = {
+#     '3': 0.104,
+#     '4': 0.78,
+#     '5': 1.42,
+#     '6': 2.08,
+#     '7': 2.73,
+#     '8': 3.38,
+#     '9': 4.03,
+#     '10': 4.68,
+#     '11': 5.33,
+#     '12': 5.98,
+#     '13': 6.63,
+#     '14': 7.28,
+#     '15': 7.93,
+#     }
+
+# Steady state effluent P in mg/L, when Q=1000 m3/d
+# S_P_dct = {
+#     '3': 0.103786314929568,
+#     '4': 0.757374963832081,
+#     '5': 1.38383912772804,
+#     '6': 2.03682213914134,
+#     '7': 2.68256462847412,
+#     '8': 3.32964385968618,
+#     '9': 3.9775093368395,
+#     '10': 4.68,
+#     '11': 5.33,
+#     '12': 5.98,
+#     '13': 6.63,
+#     '14': 7.28,
+#     '15': 7.93,
+#     }
+
+
+def create_flowsheets(inf_range=all_infs, Q=1000, init_conds=default_init_conds):
+    f_dct = dict.fromkeys(f'f{n}' for n in inf_range) 
     for k, v in f_dct.items():
         mgL = k[1:]
         path = os.path.join(data_path, f'biowin_effluent_ph{mgL}.tsv')        
@@ -63,24 +174,27 @@ def create_flowsheets(f_dct=f_dct, inf_range=range(3, 16), Q=50): # inf=3-15 mg/
         df.to_csv(new_path, sep='\t')
         # Create the EcoRecover system
         f = qs.Flowsheet(f'f{mgL}')
-        pm2.create_system(flowsheet=f)
+        pm2.create_system(flowsheet=f, init_conds=init_cond_dct[mgL])
         SE = f.unit.SE
         SE._init_from_file(new_path)
         os.remove(new_path)
+        X = max(1, Q/449*init_cond_dct[mgL]['S_P']/default_init_conds['S_P']) # to account for the increased P conc. and flowrate, 449 m3/d was EcoRecover baseline
+        if round(X, 1) > 1.1:
+            for u in f.unit:
+                if 'PBR' in u.ID:
+                    u.V_max = V_pbr_indv_default * X
+                    u._compile_ODE()
         f_dct[k] = f
     return f_dct
-
-# f_dct = create_flowsheets() # this create all the flowsheets so takes some time
 
 
 # %%
 
 # Function to run all concentrations
-def simulate_system(f_dct=None, inf_range=range(3, 16), Q=50, t_step=1, t=25, export=True):
-    f_dct = create_flowsheets(f_dct, inf_range=inf_range, Q=Q)
-    for k, v in f_dct.items():
-        mgL = k[1:]
-        f = f_dct[f'f{mgL}']
+def simulate_system(f_dct, t_step=1, t=25, export=True):
+    simulation_kwargs = dict(t_step=t_step, t=t, export=export)
+    @time_printer
+    def single_simulation(flowsheet, **simulation_kwargs):
         sys = f.system.sys
         sys.reset_cache()
         sys.simulate(t_span=(0, t), method='RK23')
@@ -89,14 +203,23 @@ def simulate_system(f_dct=None, inf_range=range(3, 16), Q=50, t_step=1, t=25, ex
         eff = f.stream.Effluent
         fig, axis = eff.scope.plot_time_series(('S_P'))
         if export:
-            fig.savefig(os.path.join(figures_path, f'ecorecover_results_{mgL}mgL_{t}d_{Q}m3d.jpg'))
-            sys.scope.export(os.path.join(results_path, f'ecorecover_results_{mgL}mgL_{t}d_{Q}m3d.xlsx'), t_eval=np.arange(0, t+t_step, t_step))
+            now = datetime.datetime.now()
+            time = f'{now.year}-{now.month}-{now.day}-{now.hour}-{now.minute}-{now.second}'
+            fig.savefig(os.path.join(figures_path, f'ecorecover_results_{mgL}mgL_{t}d_{Q}m3d_{time}.jpg'))
+            sys.scope.export(os.path.join(results_path, f'ecorecover_results_{mgL}mgL_{t}d_{Q}m3d_{time}.xlsx'), t_eval=np.arange(0, t+t_step, t_step))
         print(f'\n\nfinished {k}\n\n')
+    for k, v in f_dct.items():
+        mgL = k[1:]
+        f = f_dct[f'f{mgL}']
+        single_simulation(flowsheet=f, **simulation_kwargs)
     return f_dct
 
 
 # Individual trial
-f_dct = simulate_system(f_dct=None, inf_range=range(15, 16), Q=50, t_step=1, t=25, export=True)
+f_dct = create_flowsheets(inf_range=range(15, 16), Q=1000)
+f = f_dct['f15']
+sys = f.system.sys
+f_dct = simulate_system(f_dct=f_dct, t_step=1, t=100, export=True)
 
 # Run all results
 # f_dct = simulate_system(f_dct=None, inf_range=range(3, 16), t_step=1, t=25, export=True)
