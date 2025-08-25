8dc1d87f82dd77503a8f6e776ee452a6eab29dff
BlueS928 <buaishi2@illinois.edu>
2025-08-25 14:31:05 -0500

    redox-ed unit

--- exposan/redox_ED/_sanunits.py
+++ exposan/redox_ED/_sanunits.py
@@ -7,11 +7,13 @@ Created on Tue Aug 12 11:27:03 2025
 """
 
 from qsdsan import SanUnit,Construction, WasteStream
+from qsdsan.utils import ospath, data_path, load_data, price_ratio
 from numpy import exp
 
 __all__ = ('redox_ED',)
 
 #%%
+redox_ED_path = ospath.join(data_path, 'sanunit_data/_redox_ED.csv')
 class redox_ED(SanUnit):
     '''
     Redox-electrodialysis unit to upconcentration volatile fatty acids from waste.
@@ -55,17 +57,9 @@ class redox_ED(SanUnit):
     
     # !!!
     # Linear regression coefficients to extrpolate flux, calculated in excel
-    # and imported as class variables here. Change way to calculate and import 
-    # in future
-    c3_slope = 0.149374138
-    c3_const = 0.307055862
-    c4_slope = 0.267336207
-    c4_const = 0.155695793
-    c6_slope = 0.236910345
-    c6_const = 0.088617655
-    dimension = 4 # Unit is cm
-    thickness = 3 # Unit is cm
-    flowrate = 0.5 # ml/min
+    # and imported as instance attributes. Linear regression coefficients
+    # should be updated when new experimental data is used for future instances
+    
     
     def __init__(self, ID='', ins=None, outs=(), thermo=None, init_with='WasteStream',
                  voltage=None, op_time=None, fc_c3=None, fc_c4=None, fc_c6=None, 
@@ -73,13 +67,23 @@ class redox_ED(SanUnit):
         SanUnit.__init__(self, ID, ins, outs, thermo=thermo, init_with=init_with,
                          F_BM_default=1)
         # Defining instance attributes, operation related for redox-ED
-        # When are they assigned, because this script is only run implicitly
-        # when a system is created
+        # When do the users input values? because this script is only run 
+        # implicitly when a system is created
+        
         self.voltage = voltage
         self.operation_time = op_time
         self.fc_c3 = fc_c3
         self.fc_c4 = fc_c4
         self.fc_c6 = fc_c6
+        
+        data = load_data(path=redox_ED_path)
+        for para in data.index:
+            value = float(data.loc[para]['expected'])
+            setattr(self, '_'+para, value)
+        del data
+
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
         
     def _run(self):
         fc_in, ac_in = self.ins
@@ -94,25 +98,46 @@ class redox_ED(SanUnit):
         # !!! how to reduce repetitiveness? when flux extrpolation constants
         # are imported in other ways instead of as class attributes in future,
         # need to change ways to retrieve this data as well
-        c3_flux = redox_ED.c3_slope*self.voltage + redox_ED.c3_const
-        c4_flux = redox_ED.c4_slope*self.voltage + redox_ED.c4_const
-        c6_flux = redox_ED.c6_slope*self.voltage + redox_ED.c6_const
+        c3_flux = self.c3_slope*self.voltage + self.c3_const
+        c4_flux = self.c4_slope*self.voltage + self.c4_const
+        c6_flux = self.c6_slope*self.voltage + self.c6_const
         
-        # Calculate concentration
-        fc_out.imass['propionate'] = fc_in.imass['propionate'] + \
-        c3_flux * redox_ED.dimension**2 / redox_ED.flowrate - \
-        exp(-redox_ED.flowrate / (redox_ED.dimension ** 2 * redox_ED.thickness) 
-            *self.operation_time + c3_flux * redox_ED.dimension ** 2 - 1)
+        area = self.dimension**2
+        volume = self.dimension ** 2 * self.thickness
         
-        fc_out.imass['butyrate'] = fc_in.imass['butyrate'] + \
-        c4_flux * redox_ED.dimension**2 / redox_ED.flowrate - \
-        exp(-redox_ED.flowrate / (redox_ED.dimension ** 2 * redox_ED.thickness) 
-            *self.operation_time + c4_flux * redox_ED.dimension ** 2 - 1)
+        # Calculate accumulating channel effluent concentration
+        ac_out.imass['propionate'] = ac_in.imass['propionate'] + \
+        c3_flux * area / self.flowrate - \
+        exp(-self.flowrate / volume 
+            *self.operation_time + c3_flux * self.area - 1)
+        ac_out.imass['butyrate'] = ac_in.imass['butyrate'] + \
+        c4_flux * area / self.flowrate - \
+        exp(-self.flowrate / volume 
+            *self.operation_time + c4_flux * self.area - 1)
+        ac_out.imass['hexanoate'] = ac_in.imass['hexanoate'] + \
+        c6_flux * area / self.flowrate - \
+        exp(-self.flowrate / volume 
+            *self.operation_time + c6_flux * self.area - 1)
         
-        fc_out.imass['hexanoate'] = fc_in.imass['hexanoate'] + \
-        c6_flux * redox_ED.dimension**2 / redox_ED.flowrate - \
-        exp(-redox_ED.flowrate / (redox_ED.dimension ** 2 * redox_ED.thickness) 
-            *self.operation_time + c6_flux * redox_ED.dimension ** 2 - 1)
+        # Calcualte feeding channel effluent concentration        
+        fc_out.imass['propionate'] = fc_in.imass['propionate'] - \
+        c3_flux * area / self.flowrate - \
+        exp(-self.flowrate / volume 
+            *self.operation_time - c3_flux * self.area - 1)
+        fc_out.imass['butyrate'] = fc_in.imass['butyrate'] - \
+        c4_flux * area / self.flowrate - \
+        exp(-self.flowrate / volume 
+            *self.operation_time - c4_flux * self.area - 1)
+        fc_out.imass['hexanoate'] = fc_in.imass['hexanoate'] - \
+        c6_flux * area / self.flowrate - \
+        exp(-self.flowrate / volume 
+            *self.operation_time - c6_flux * self.area - 1)
+
+    def _init_lca(self): 
+        self.construction = [Construction()]
+    def _design(self):
+        design = self.design_results
+        constr = self.construction
         
         
         
