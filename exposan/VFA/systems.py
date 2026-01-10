b4b7e59ec5c840d86413f9447474b3a129e57e40
BlueS928 <buaishi2@illinois.edu>
2026-01-10 20:03:02 +0800

    deleted init.py

--- exposan/VFA/systems.py
+++ exposan/VFA/systems.py
@@ -8,6 +8,7 @@ Created on Fri Sep 12 14:46:31 2025
 
 import qsdsan as qs
 from qsdsan import WasteStream
+from qsdsan.utils import ospath, data_path
 from exposan.VFA._components import create_components
 from exposan.VFA import _sanunits_copy as su
 from qsdsan import sanunits as qsu
@@ -49,7 +50,7 @@ AC_influent = WasteStream('AC_influent', Na=13.67, Cl=21.08, K=0, H2O=7743.28,
 
 
 #%%
-# Creating SanUnit instances
+### Creating SanUnit instances
 Centrifuge = su.SolidsSeparation('Centrifuge', ins = (AD_effluent, 'polymer'), outs = ('liquid_stream', 'solids_stream'))
 # the second influent of the SolidsSeparation unit is polymer, which is calculated based on the first influent, we would not need to define a separate stream for polymer. Polymer requirement will be calculated from ws0
 Centrifuge.ins[1].price = 1.3 / 0.453592 * qs.CEPCI_by_year[2023] / qs.CEPCI_by_year[2014] #unit price of polymer in $/lb from CapdetWorks 2014 database, convert from lb to kg, not including Ferric Chloride cost
@@ -58,97 +59,43 @@ Microfiltration = qsu.SludgeThickening('Microfiltration', ins = Centrifuge-0, ou
 BasePump = su.BaseDosing('BasePump', ins = (Microfiltration-0, 'base'), outs = ('effluent',))
 RedoxED = su.RedoxED('RedoxED', ins = (BasePump-0, AC_influent), outs = ('feeding_channel_out', 'accumulating_channel_out'), voltage = 1)
 
-# Creating system
+### Creating system
 sys = qs.System.from_units(ID = 'sys', units = [Centrifuge, Microfiltration, BasePump, RedoxED])
 
-
-
-
-
-
-    
-flowsheet_ID = 'htl_geospatial'
-
+flowsheet_ID = 'vfa_separation'
 # clear flowsheet and registry for reloading
 if hasattr(qs.main_flowsheet.flowsheet, flowsheet_ID):
     getattr(qs.main_flowsheet.flowsheet, flowsheet_ID).clear()
-    clear_lca_registries()
+    qs.clear_lca_registries()
 flowsheet = qs.Flowsheet(flowsheet_ID)
 stream = flowsheet.stream
 qs.main_flowsheet.set_flowsheet(flowsheet)
 
-_load_components()
-_load_process_settings(location=state)
 
-folder = '/Users/jiananfeng/Desktop/PhD_CEE/NSF_PFAS/HTL_geospatial/'
-qs.ImpactIndicator.load_from_file(os.path.join(folder, 'data/impact_indicators.csv'))
-qs.ImpactItem.load_from_file(os.path.join(folder, 'data/impact_items.xlsx'))
-
-raw_wastewater = qs.WasteStream('sludge_assumed_in_wastewater', H2O=size, units='MGD', T=25+273.15)
-# set H2O equal to the total raw wastewater into the WWTP
+### load construction impact
+qs.ImpactIndicator.load_from_file(ospath.join(data_path, 'sanunit_data/VFA/impact_indicators.csv'))
+qs.ImpactItem.load_from_file(ospath.join(data_path, 'sanunit_data/VFA/impact_items.xlsx'))
 
 
+### add stream impact items
 
+# add impact for polymer
+qs.StreamImpactItem(ID='polymer_item',
+                    GlobalWarming=5.19)
 
-
-
-# load construction impact
-
-
-# add stream impact items
 # add impact for waste sludge
 qs.StreamImpactItem(ID='waste_sludge1_item',
-                    linked_stream=stream.sludge,
-                    Acidification=0,
-                    Ecotoxicity=0,
-                    Eutrophication=0,
-                    GlobalWarming=-WWTP.ww_2_dry_sludge*waste_GWP/3.79/(10**6),
-                    OzoneDepletion=0,
-                    PhotochemicalOxidation=0,
-                    Carcinogenics=0,
-                    NonCarcinogenics=0,
-                    RespiratoryEffects=0)
+                    GlobalWarming=0.292)
 
 qs.StreamImpactItem(ID='waste_sludge2_item',
-                    linked_stream=stream.sludge,
-                    Acidification=0,
-                    Ecotoxicity=0,
-                    Eutrophication=0,
-                    GlobalWarming=-WWTP.ww_2_dry_sludge*waste_GWP/3.79/(10**6),
-                    OzoneDepletion=0,
-                    PhotochemicalOxidation=0,
-                    Carcinogenics=0,
-                    NonCarcinogenics=0,
-                    RespiratoryEffects=0)
+                    GlobalWarming=0.292)
 
 # add impact of pH adjustment
 qs.StreamImpactItem(ID='NaOH_item',
-                    linked_stream=stream.NaOH,
-                    Acidification=0.33656,
-                    Ecotoxicity=0.77272,
-                    Eutrophication=0.00032908,
-                    GlobalWarming=1.2514,
-                    OzoneDepletion=7.89E-07,
-                    PhotochemicalOxidation=0.0033971,
-                    Carcinogenics=0.0070044,
-                    NonCarcinogenics=13.228,
-                    RespiratoryEffects=0.0024543)
+                    GlobalWarming=1.2514)
     
 
-
-
-
-    
-
-    
-
-    
-
-    
-
-    
-
-_tea.create_tea(sys, IRR_value=0.03, income_tax_value=0.21, finance_interest_value=0.03, labor_cost_value=29.32)
+_tea.create_tea(sys, IRR_value=0.03, income_tax_value=0.21, finance_interest_value=0.03, labor_cost_value = 29.32)
 
 qs.LCA(system=sys, lifetime=30, lifetime_unit='yr',
        Electricity=lambda:(sys.get_electricity_consumption()-sys.get_electricity_production())*30,
