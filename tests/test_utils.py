8c9f5782b5717ef0770770932f71e9b2387ff13f
Yalin Li <mailto.yalin.li@gmail.com>
2026-07-03 22:23:57 -0400

    feat: add require_package helper; make sklearn import lazy in exposan.utils

---
+++ tests/test_utils.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+EXPOsan: Exposition of sanitation and resource recovery systems
+
+This module is under the University of Illinois/NCSA Open Source License.
+Please refer to https://github.com/QSD-Group/EXPOsan/blob/main/LICENSE.txt
+for license details.
+'''
+
+import pytest
+
+__all__ = (
+    'test_require_package_passes_for_installed_package',
+    'test_require_package_raises_friendly_error_for_missing_package',
+    'test_get_generic_tanker_truck_fee',
+    'test_get_generic_tanker_truck_fee_raises_friendly_error_without_sklearn',
+    )
+
+
+def test_require_package_passes_for_installed_package():
+    from exposan.utils import require_package
+
+    # numpy is always installed (transitive via qsdsan); must not raise
+    require_package('numpy')
+
+
+def test_require_package_raises_friendly_error_for_missing_package():
+    from exposan.utils import require_package
+
+    with pytest.raises(ModuleNotFoundError) as exc_info:
+        require_package(
+            'a-package-that-does-not-exist-1234',
+            feature='a made-up feature',
+        )
+    msg = str(exc_info.value)
+    assert 'a made-up feature' in msg
+    assert 'a-package-that-does-not-exist-1234' in msg
+    assert 'pip install a-package-that-does-not-exist-1234' in msg
+    assert 'pip install exposan[complete]' in msg
+
+
+def test_get_generic_tanker_truck_fee():
+    from exposan.utils import get_generic_tanker_truck_fee
+
+    fee = get_generic_tanker_truck_fee(6)
+    assert fee > 0
+
+
+def test_get_generic_tanker_truck_fee_raises_friendly_error_without_sklearn(monkeypatch):
+    import exposan.utils as exposan_utils
+    from exposan.utils import get_generic_tanker_truck_fee
+
+    real_import_module = exposan_utils.importlib.import_module
+    def fake_import_module(name, *args, **kwargs):
+        if name == 'sklearn':
+            raise ModuleNotFoundError(name='sklearn')
+        return real_import_module(name, *args, **kwargs)
+    monkeypatch.setattr(exposan_utils.importlib, 'import_module', fake_import_module)
+
+    with pytest.raises(ModuleNotFoundError) as exc_info:
+        get_generic_tanker_truck_fee(6)
+    msg = str(exc_info.value)
+    assert 'scikit-learn' in msg
+    assert 'pip install exposan[complete]' in msg
